aa7a3cb2197a69f3422ae52f4e82d12c034f005f
Jollen Chen <jollen@moko365.com>
2017-07-11 16:20:17 +0800

    get local IP address

--- app/app.js
+++ app/app.js
@@ -14,14 +14,43 @@ var wsServer = WoT.Server.WebsocketBroker;
 process.env.PORT = 8000;
 process.env.HOST = '127.0.0.1';
 
+// Get local IP address (IPv4)
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+    } else {
+      // this interface has only one ipv4 adress
+      process.env.HOST = iface.address;
+      return;
+    }
+    ++alias;
+  });
+});
+
 var ondata = function(payload) {
 	var obj = JSON.parse(payload.data);
 	var paths = payload.pathname.split('/');
 	var deviceId = paths[2];
 
- 	$('#status').html(obj.temperature);
+ 	$('#data').html(obj.temperature);
+};
+
+var onstart = function(payload) {
+	$('#status').html("Server is at " + process.env.HOST + ":" + process.env.PORT );
 };
 
 wsServer.start({
+	onstart: onstart,
 	ondata: ondata
 });
